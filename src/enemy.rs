3295f59500b7f26fb89644f3f9fae415c1b89041
Bibazavr <lirik.hariev@gmail.com>
2024-06-23 14:53:17 +0300

    feat: add enemy_move

--- src/enemy.rs
+++ src/enemy.rs
@@ -2,10 +2,11 @@ use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 use rand::random;
 
-// This is the player sprite size.
 use crate::assets_loader::SceneAssets;
+use crate::player::Player;
 
 pub const ENEMY_COUNT: usize = 4;
+pub const ENEMY_SPEED: f32 = 400.0;
 
 #[derive(Component)]
 pub struct Enemy {}
@@ -14,7 +15,8 @@ pub struct EnemyPlugin;
 
 impl Plugin for EnemyPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(PostStartup, spawn_enemy);
+        app.add_systems(PostStartup, spawn_enemy)
+            .add_systems(Update, move_enemy);
     }
 }
 
@@ -36,5 +38,35 @@ pub fn spawn_enemy(
             },
             Enemy {},
         ));
+    }
+}
+
+pub fn move_enemy(
+    mut enemy_query: Query<&mut Transform, With<Enemy>>,
+    player_query: Query<&mut Transform, (With<Player>, Without<Enemy>)>,
+    time: Res<Time>,
+) {
+    if let Ok(player_transform) = player_query.get_single() {
+        for mut enemy_transform in enemy_query.iter_mut() {
+            let mut direction = Vec3::ZERO;
+
+            if player_transform.translation.x > enemy_transform.translation.x {
+                direction += Vec3::new(1.0, 0.0, 0.0);
+            } else {
+                direction += Vec3::new(-1.0, 0.0, 0.0);
+            }
+
+            if (player_transform.translation.y > enemy_transform.translation.y) {
+                direction += Vec3::new(0.0, 1.0, 0.0);
+            } else {
+                direction += Vec3::new(0.0, -1.0, 0.0);
+            }
+
+            if direction.length() > 0.0 {
+                direction = direction.normalize();
+            }
+
+            enemy_transform.translation += direction * ENEMY_SPEED * time.delta_seconds();
+        }
     }
 }

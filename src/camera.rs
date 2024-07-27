1f091fc045b3d585083c66c102067ada23564f4a
Bibazavr <lirik.hariev@gmail.com>
2024-07-27 18:51:58 +0300

    feat: camera follow player

--- src/camera.rs
+++ src/camera.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 
+use crate::player::Player;
+
 #[derive(Component, Debug)]
 pub struct MainCamera;
 
@@ -7,16 +9,35 @@ pub struct MainCameraPlugin;
 
 impl Plugin for MainCameraPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_camera);
+        app.add_systems(Startup, spawn_camera)
+            .add_systems(Update, follow_player);
     }
 }
 
 fn spawn_camera(mut commands: Commands) {
     commands.spawn((
         Camera3dBundle {
-            transform: Transform::from_xyz(-3.0, 3.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+            transform: Transform::from_xyz(-10.0, 10.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
             ..Default::default()
         },
         MainCamera,
     ));
+}
+
+fn follow_player(
+    player_query: Query<&Transform, With<Player>>,
+    mut camera_query: Query<&mut Transform, (With<Camera>, Without<Player>)>,
+) {
+    let Ok(player_transform) = player_query.get_single() else {
+        return;
+    };
+    let Ok(mut camera_transform) = camera_query.get_single_mut() else {
+        return;
+    };
+    camera_transform.translation = player_transform.translation
+        + Vec3 {
+            x: -10.0,
+            y: 10.0,
+            z: 10.0,
+        }
 }
